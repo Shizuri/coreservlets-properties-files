fcf1afed81fa36f20a12398852e33f16cd5e84f7
Zdravko Mavkov <mavkov@hotmail.com>
2018-12-13 10:04:58 +0100

    added random pages, task finished

--- src/exercises/RandomLocaleController.java
+++ src/exercises/RandomLocaleController.java
@@ -1,6 +1,7 @@
 package exercises;
 
 import java.util.Locale;
+import java.util.Random;
 
 import javax.faces.bean.ManagedBean;
 
@@ -8,6 +9,15 @@ import javax.faces.bean.ManagedBean;
 public class RandomLocaleController extends One{
 
 	public Locale getRandomLocale() {
-		return(new Locale("mk"));
+		Random r = new Random();
+		int result = r.nextInt(3);
+		System.out.println("Result is: " + result);
+		if(result == 0) {
+			return(new Locale("en"));
+		} else if (result == 1) {
+			return(new Locale("es"));
+		} else {
+			return(new Locale("mk"));
+		}
 	}
 }
